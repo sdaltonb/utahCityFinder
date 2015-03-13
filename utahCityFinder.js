a9e66ef3e67cd839fd95bc92b2ab312a9aec40a0
sdaltonb <ec2-user@ip-172-31-26-146.us-west-2.compute.internal>
2015-03-13 05:14:32 +0000

    Using mongodb

--- utahCityFinder.js
+++ utahCityFinder.js
@@ -1,12 +1,60 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var MongoClient = require('mongodb').MongoClient;
 
 var ROOT_DIR = "html/";
 
 http.createServer(function (req, res) {
    var urlObj = url.parse(req.url, true, false);
-   if (urlObj.pathname.indexOf("getcity") != -1) {
+   if (urlObj.pathname.indexOf("comment") != -1) {
+      console.log("comment route");
+      if (req.method === "POST") {
+         console.log("POST comment route");
+         var jsonData = "";
+         req.on('data', function(chunk) {
+            jsonData += chunk;
+         });
+         req.on('end', function() {
+            var reqObj = JSON.parse(jsonData);
+            console.log(reqObj);
+            console.log("Name: " + reqObj.Name);
+   	    console.log("Comment: " + reqObj.Comment);
+         
+            MongoClient.connect("mongodb://localhost/weatherdb", function(err, db) {
+               if (err) {
+                  throw err;
+	       }
+	       db.collection('comments').insert(reqObj, function(err, records) {
+                  console.log("Record added as " + records[0]._id);
+               });
+            });
+            res.writeHead(200);
+            res.end("");
+         });
+      } else if (req.method === "GET") {
+         console.log("In GET");
+         MongoClient.connect("mongodb://localhost/weatherdb", function(err, db) {
+	    if (err) {
+	       throw err;
+	    }
+	    db.collection("comments", function(err, comments) {
+	       if (err) {
+		  throw err;
+	       }
+	       comments.find(function(err, items) {
+	          items.toArray(function(err, itemArr) {
+		     console.log("Document Array: ");
+		     console.log(itemArr);
+	             res.writeHead(200);
+		     res.end(JSON.stringify(itemArr));
+		  });
+	       });
+	    });
+	 });        
+      }
+   }
+   else if (urlObj.pathname.indexOf("getcity") != -1) {
       fs.readFile(ROOT_DIR + "cities.dat.txt", function (err, data) {
          if (err) {
             throw err;
